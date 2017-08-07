6c799be24acd125ae69138e0843e54950b898199
ckeenan <ckeenan89@gmail.com>
2017-08-07 13:55:05 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 'use strict';
+var protocol = process.env.APP_PROTOCOL || 'http';
 
 angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
 
@@ -28,7 +29,7 @@ angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
     .run(function($rootScope, $location) {
         var web3 = new Web3();
         var host = $location.host();
-        var eth_node_url = `http://${host}:8545`; // TODO: remote URL
+        var eth_node_url = `${protocol}://${host}:8545`; // TODO: remote URL
 	web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
         $rootScope.web3 = web3;
         function sleepFor( sleepDuration ){
